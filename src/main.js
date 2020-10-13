abc984cbe79bfd6cac46a0b22af9f5d8c13c2bee
Andrey Kalov <iamkot2@gmail.com>
2020-10-13 16:16:27 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+
+Vue.use(VueRouter)
+
+let router = new VueRouter({
+
+})
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router: router
+})
